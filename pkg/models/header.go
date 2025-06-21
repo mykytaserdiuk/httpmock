3f47f87344bd9553c2b93aad6dd163f255fd46b5
Mykyta <hiban.com@gmail.com>
2025-06-21 20:14:04 +0200

    update request checking

--- pkg/models/header.go
+++ pkg/models/header.go
@@ -1,6 +1,11 @@
 package models
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+	"reflect"
+	"sort"
+)
 
 // Header can have expected properties
 // TODO or some logic
@@ -12,4 +17,27 @@ func (h Header) WriteTo(w http.ResponseWriter) {
 			w.Header().Add(key, v)
 		}
 	}
+}
+
+func (h Header) IsEquals(r *http.Request) error {
+	actual := r.Header
+
+	for key, expectedVals := range h {
+		actualVals, ok := actual[key]
+		if !ok {
+			return errors.New("missing header key: '" + key + "' in actual response")
+		}
+
+		expected := append([]string{}, expectedVals...)
+		actualCopy := append([]string{}, actualVals...)
+
+		sort.Strings(expected)
+		sort.Strings(actualCopy)
+
+		if !reflect.DeepEqual(expected, actualCopy) {
+			return errors.New("header values for key '" + key + "' do not match")
+		}
+	}
+
+	return nil
 }
